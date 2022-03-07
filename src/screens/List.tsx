0ea1d29062e03915efaee1eafb787e6b537144e6
Nick Alekhine <nick.alekhine@gmail.com>
2022-03-06 19:27:01 -0600

    clean it up

--- src/screens/List.tsx
+++ src/screens/List.tsx
@@ -1,5 +1,5 @@
-import {StackScreenProps, useGestureHandlerRef} from '@react-navigation/stack';
-import React, {createRef, useRef, useState} from 'react';
+import {StackScreenProps} from '@react-navigation/stack';
+import React, {createRef, useState} from 'react';
 import {
   ListRenderItemInfo,
   Text,
@@ -10,11 +10,9 @@ import {
   FlatList,
   Gesture,
   GestureDetector,
-  PanGestureHandler,
   ScrollView,
-  TapGestureHandler,
 } from 'react-native-gesture-handler';
-import Animated, {runOnJS, useSharedValue} from 'react-native-reanimated';
+import {runOnJS, useSharedValue} from 'react-native-reanimated';
 import {RootStackParamList} from '../App';
 
 type Props = StackScreenProps<RootStackParamList, 'List'>;
@@ -23,21 +21,11 @@ const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const data2 = [1000, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200];
 
 export default function ListScreen(_: Props) {
-  const ref = useGestureHandlerRef();
-  const scrollRef = createRef<ScrollView>();
-
   const {width} = useWindowDimensions();
 
+  const scrollRef = createRef<ScrollView>();
   const startingX = useSharedValue<number>(0);
-  const offsetX = useSharedValue<number>(0);
-  const accOffsets = useSharedValue<number>(0);
-
   const [posn, setPosn] = useState<number>(0);
-
-  const wf = useRef<[typeof ref] | []>([]);
-
-  const lezSet = () => (wf.current = [ref]);
-  const lezUnset = () => (wf.current = []);
 
   const lezUnset2 = () => {
     scrollRef.current?.setNativeProps({scrollEnabled: false});
@@ -53,15 +41,10 @@ export default function ListScreen(_: Props) {
     })
     .onTouchesMove((e, sm) => {
       const oX = e.changedTouches[0].absoluteX - startingX.value;
-      if (accOffsets.value + oX > 0 && posn === 0) {
-        console.log('1');
-        accOffsets.value = 0;
-        offsetX.value = 0;
+      if (oX > 0 && posn === 0) {
         sm.fail();
         runOnJS(lezUnset2)();
       } else {
-        console.log('2');
-        offsetX.value = oX;
         runOnJS(lezSet2)();
       }
     });
